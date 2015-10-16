b8e4de57afbc6ecf28a9e0ff95c3a1e80069b217
amourav <souravsrkr254@gmail.com>
2015-10-17 03:16:06 +0530

    courses by users working

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016205606) do
+ActiveRecord::Schema.define(version: 20151016212842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "code"
+    t.string   "name"
+    t.string   "year"
+    t.string   "semester"
+    t.integer  "teacher_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "teachers", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
